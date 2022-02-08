eb29cc4f00c65b43d4505a3e823f2f64fc57957f
inanc <inanc@DESKTOP-14AOJ2S>
2022-02-08 15:59:58 +0300

    Add project files.

---
+++ DrawToFourier/DrawToFourierApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace DrawToFourier
+{
+
+    internal class DrawToFourierApp : Application
+    {
+        private Window _drawWindow;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Starting app...");
+            InitApp();
+        }
+
+        public static void InitApp()
+        {
+            DrawToFourierApp app = new DrawToFourierApp();
+            app.Run();
+        }
+
+        public DrawToFourierApp() : base()
+        {
+            this.Startup += AppStartupHandler;
+        }
+
+        private void AppStartupHandler(object sender, StartupEventArgs e)
+        {
+            this.MainWindow = this._drawWindow = new DrawWindow();
+            this.MainWindow.Show();
+        }
+
+    }
+}
