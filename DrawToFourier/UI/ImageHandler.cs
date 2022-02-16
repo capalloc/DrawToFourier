c9cbdedf1734efe3da6a77ce32873f31c81d44af
Umut İnanç <umut.inanc@ogr.deu.edu.tr>
2022-02-16 17:53:36 +0300

    Can draw as moved

--- DrawToFourier/UI/ImageHandler.cs
+++ DrawToFourier/UI/ImageHandler.cs
@@ -155,18 +155,33 @@ namespace DrawToFourier.UI
             ProgramAction += programAction;
         }
 
+        private Point? last;
+        private bool started = false;
+
         public override void OnMouseDown(double X, double Y, MouseButton clicked)
         {
             if (this.ProgramAction != null)
             {
+                if (!started)
+                {
+                    this.started = true;
+                    this.last = new Point(X, Y);
+                }
+                
                 this.ProgramAction.Invoke(this, new CoreProgramActionEventArgs("Down", X, Y));
             }
         }
 
         public override void OnMouseLeave(double X, double Y)
         {
-            if (this.ProgramAction != null)
+            if (this.ProgramAction != null && last != null)
             {
+                if (started)
+                {
+                    Point p = new Point(X, Y);
+                    DrawLine(this._bmp, (Point) last, p);
+                    last = null;
+                }
                 this.ProgramAction.Invoke(this, new CoreProgramActionEventArgs("Leave", X, Y));
             }
         }
@@ -175,6 +190,10 @@ namespace DrawToFourier.UI
         {
             if (this.ProgramAction != null)
             {
+                if(started)
+                {
+                    this.last = new Point(X, Y);
+                }
                 this.ProgramAction.Invoke(this, new CoreProgramActionEventArgs("Enter", X, Y));
             }
         }
@@ -183,6 +202,12 @@ namespace DrawToFourier.UI
         {
             if (this.ProgramAction != null) 
             {
+                if (started && this.last != null)
+                {
+                    Point p = new Point(X, Y);
+                    DrawLine(this._bmp, (Point) last, p);
+                    last = p;
+                }
                 this.ProgramAction.Invoke(this, new CoreProgramActionEventArgs("Move", X, Y)); 
             }
         }
